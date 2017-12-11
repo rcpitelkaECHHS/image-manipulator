3ffda21820a4be78b0b8280b1f2bc3cd2d82f0d6
rcpitelkaECHHS <rcpitelka@students.chccs.k12.nc.us>
2017-12-11 10:10:05 -0500

    Image Files Setup and Test
    Added imaage files and defined a test function to show the logo

--- RCP 1.4.7 Image Manipulator.py
+++ RCP 1.4.7 Image Manipulator.py
@@ -1,4 +1,9 @@
 import PIL
 import matplotlib.pyplot as plt # single use of plt is commented out
 import os.path  
-import PIL.ImageDraw           
+import PIL.ImageDraw
+
+def show_logo(logo_file):
+    logo = PIL.Image.open(logo_file)
+    plt.imshow(logo)
+    
