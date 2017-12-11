78996582602a4e09aa19fc69029b5583491f3a07
rcpitelkaECHHS <rcpitelka@students.chccs.k12.nc.us>
2017-12-11 10:24:25 -0500

    Image Files Setup and Test Pt. 2
    Added another logo option and defined a test function to show the images
    in the 'Image Files' folder.

--- RCP 1.4.7 Image Manipulator.py
+++ RCP 1.4.7 Image Manipulator.py
@@ -7,3 +7,6 @@ def show_logo(logo_file):
     logo = PIL.Image.open(logo_file)
     plt.imshow(logo)
     
+def show_image(image_file):
+    image = PIL.Image.open(os.getcwd() + '/Image Files/' + image_file)
+    plt.imshow(image)
