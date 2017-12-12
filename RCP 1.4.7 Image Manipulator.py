ae68e4963742c6821a0323f0bbb96190b2fb64f5
rcpitelkaECHHS <rcpitelka@students.chccs.k12.nc.us>
2017-12-12 10:28:09 -0500

    Change to matplot Images and Main Function Creation
    Changed from PIL images to matplot images. Began writing main image
    filter, and got the black and white filter working correctly, except not
    in the right location on the image.

--- RCP 1.4.7 Image Manipulator.py
+++ RCP 1.4.7 Image Manipulator.py
@@ -4,9 +4,20 @@ import os.path
 import PIL.ImageDraw
 
 def show_logo(logo_file):
-    logo = PIL.Image.open(logo_file)
+    logo = plt.imread(logo_file)
     plt.imshow(logo)
     
 def show_image(image_file):
-    image = PIL.Image.open(os.getcwd() + '/Image Files/' + image_file)
+    image = plt.imread(os.getcwd() + '/Image Files/' + image_file)
+    plt.imshow(image)
+
+def dramatize_image(image_file, logo=None):
+    image = plt.imread(os.getcwd() + '/Image Files/' + image_file)
+    height = len(image)
+    width = len(image[0])
+    for r in range(0, height):
+        for c in range(1, width):
+            if r/c > (height/width):
+                color_value = int(round(sum(image[r][c])/3))
+                image[r][c] = (color_value, color_value, color_value)
     plt.imshow(image)
